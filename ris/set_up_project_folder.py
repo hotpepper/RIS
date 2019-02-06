35a0f25618afe895e833f38a5a927b60ee99ac4b
SHostetter <aasere@gmail.com>
2019-02-06 18:44:56 -0500

    put in function with folder path passed as param

--- ris/set_up_project_folder.py
+++ ris/set_up_project_folder.py
@@ -1,36 +1,41 @@
 import os
 
+def make_project_folder(path):
+    os.chdir(path)
+    print 'Adding project structure to {}'.format(path)
+    # add ignore file
+    f = open('.gitignore', 'w')
+    exts = [
+        '*.idea',
+        '*.pyc',
+        '*.mdb',
+        '*.csv',
+        '*.xls',
+        '*.xlsx',
+        '*.shp',
+        '*.cpg',
+        '*.dbf',
+        '*.prj',
+        '*.sbn',
+        '*.sbx',
+        '*.xml',
+        '*.zip',
+        r'DATA/**',
+        r'ARCHIVE/**',
+        r'MISC/**'
+    ]
+    for i in exts:
+        f.write(i + '\n')
+    f.close()
 
-# add ignore file
-f = open('.gitignore', 'w')
-exts = [
-    '*.idea',
-    '*.pyc',
-    '*.mdb',
-    '*.csv',
-    '*.xls',
-    '*.xlsx',
-    '*.shp',
-    '*.cpg',
-    '*.dbf',
-    '*.prj',
-    '*.sbn',
-    '*.sbx',
-    '*.xml',
-    '*.zip',
-    r'DATA/**',
-    r'ARCHIVE/**',
-    r'MISC/**'
-]
-for i in exts:
-    f.write(i+'\n')
-f.close()
+    folders = [
+        'DATA',
+        'ARCHIVE',
+        'DOCUMENTATION',
+        'MISC'
+    ]
+    for i in folders:
+        if not os.path.isdir(os.path.join(path, i)):
+            os.makedirs(i)
 
-folders = [
-    'DATA',
-    'ARCHIVE',
-    'DOCUMENTATION',
-    'MISC'
-]
-for i in folders:
-    os.makedirs(i)
+    os.startfile(path)
